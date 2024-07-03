9e3f2a2dc80b194e4d186214533eeea77e55509a
Theodore <theodore@localhost.localdomain>
2024-07-02 19:49:08 -0500

    Add solved Rustlings exercises

---
+++ Rustlings/exercises/clippy/target/debug/deps/clippy3-94c4182c5bdd363c.d
@@ -0,0 +1,9 @@
+/home/theodore/Documents/Projects/RustStuff/rustlings/exercises/clippy/target/debug/deps/libclippy3-94c4182c5bdd363c.rmeta: clippy3.rs Cargo.toml
+
+/home/theodore/Documents/Projects/RustStuff/rustlings/exercises/clippy/target/debug/deps/clippy3-94c4182c5bdd363c.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ Rustlings/exercises/variables/variables6.rs
@@ -0,0 +1,9 @@
+// variables6.rs
+//
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
+// hint.
+
+const NUMBER : u16 = 420;
+fn main() {
+    println!("Number {}", NUMBER);
+}
